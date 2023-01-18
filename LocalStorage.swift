70c07202ffa9c5aae65362452c8fa3c6235dcc0f
Florian Hubl <49409175+FlorianHubl@users.noreply.github.com>
2023-01-18 09:28:28 +0100

    LocalStorage

---
+++ LocalStorage.swift
@@ -0,0 +1,52 @@
+//  Created by Florian Hubl on 18.01.2023
+
+import SwiftUI
+
+@propertyWrapper
+struct LocalStorage<Item: Codable>: DynamicProperty {
+    @State private var encoded: Item
+    let key: String
+    
+    var wrappedValue: Item {
+        get {
+            encoded
+        }
+        nonmutating set {
+            encoded = newValue
+            let json = try! JSONEncoder().encode(newValue)
+            UserDefaults().set(json, forKey: key)
+        }
+    }
+    
+    var projectedValue: Binding<Item> {
+        Binding(
+            get: { wrappedValue },
+            set: { wrappedValue = $0 }
+        )
+    }
+    
+    init(wrappedValue: Item, _ a: String) {
+        let data = UserDefaults().data(forKey: a)
+        self.key = a
+        if let data = data {
+            do {
+                let item = try JSONDecoder().decode(Item.self, from: data)
+                self._encoded = State(wrappedValue: item)
+            }catch {
+                self._encoded = State(wrappedValue: wrappedValue)
+                let json = try! JSONEncoder().encode(wrappedValue)
+                UserDefaults().set(json, forKey: a)
+                print("LocalStorage: Input has not the correct type")
+                print("Input key: \(a)")
+                print("Input item: \(wrappedValue)")
+                print("Input type: \(type(of: wrappedValue))")
+                print("Data in UserDefaults: \(String(data: data, encoding: .utf8) ?? "Data Error")")
+                print("The data has been overwritten with: \(wrappedValue)")
+            }
+        }else {
+            self._encoded = State(wrappedValue: wrappedValue)
+            let json = try! JSONEncoder().encode(wrappedValue)
+            UserDefaults().set(json, forKey: a)
+        }
+    }
+}
