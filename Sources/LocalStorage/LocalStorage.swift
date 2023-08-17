58ea8d003621d0f26c9d25b6cd3a63e2554142b0
Florian Hubl <florian.hubl@ewe.net>
2023-08-17 16:14:59 +0200

    update

--- Sources/LocalStorage/LocalStorage.swift
+++ Sources/LocalStorage/LocalStorage.swift
@@ -2,17 +2,17 @@
 
 import SwiftUI
 
-@available(iOS 13.0, *)
+@available(iOS 14.0, *)
 @propertyWrapper
-public struct LocalStorage<Item: Codable>: DynamicProperty {
-    @State private var encoded: Item
+public class LocalStorage<Item: Codable>: DynamicProperty {
+    @Published private var encoded: Item
     let key: String
     
     public var wrappedValue: Item {
         get {
             encoded
         }
-        nonmutating set {
+        set {
             encoded = newValue
             let json = try! JSONEncoder().encode(newValue)
             UserDefaults().set(json, forKey: key)
@@ -21,8 +21,8 @@ public struct LocalStorage<Item: Codable>: DynamicProperty {
     
     public var projectedValue: Binding<Item> {
         Binding(
-            get: { wrappedValue },
-            set: { wrappedValue = $0 }
+            get: { self.wrappedValue },
+            set: { self.wrappedValue = $0 }
         )
     }
     
@@ -32,10 +32,10 @@ public struct LocalStorage<Item: Codable>: DynamicProperty {
         if let data = data {
             do {
                 let item = try JSONDecoder().decode(Item.self, from: data)
-                self._encoded = State(wrappedValue: item)
+                self._encoded = Published(wrappedValue: item)
             }catch {
-                self._encoded = State(wrappedValue: wrappedValue)
                 let json = try! JSONEncoder().encode(wrappedValue)
+                self._encoded = Published(wrappedValue: wrappedValue)
                 UserDefaults().set(json, forKey: a)
                 print("LocalStorage: Input has not the correct type")
                 print("Input key: \(a)")
@@ -45,7 +45,7 @@ public struct LocalStorage<Item: Codable>: DynamicProperty {
                 print("The data has been overwritten with: \(wrappedValue)")
             }
         }else {
-            self._encoded = State(wrappedValue: wrappedValue)
+            self._encoded = Published(wrappedValue: wrappedValue)
             let json = try! JSONEncoder().encode(wrappedValue)
             UserDefaults().set(json, forKey: a)
         }
